30b9c1644182e11f5d9cb728daa8d29be8f4ffaa
Eric Storey <estorey@gmail.com>
2019-04-12 21:32:08 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -31,7 +31,10 @@ end
 def select_series_title_with_most_human_characters
   "SELECT series.title
   FROM series 
-  JOIN "
+  JOIN characters ON characters.series_id = series.id
+  GROUP BY series
+  WHERE characters.species = 'human'
+  ;"
 end
 
 def select_character_names_and_number_of_books_they_are_in
